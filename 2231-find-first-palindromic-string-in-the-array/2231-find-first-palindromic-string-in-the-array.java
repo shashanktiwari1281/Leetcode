bff4cbc9d898977bdff1b135bf23f8258ccaf891
Shashank Tiwari <115130762+shashanktiwari1281@users.noreply.github.com>
2025-05-10 06:47:48 +0530

    Time: 2 ms (57.11%), Space: 45.5 MB (8.64%) - LeetHub

--- 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.java
@@ -4,6 +4,7 @@ class Solution {
             for( int i=0,j=s.length()-1;i<=j;i++,j--){
                 if(i==j||(i==j-1 && s.charAt(i)==s.charAt(j))) return s;
                 else if(s.charAt(i)!=s.charAt(j)) break;
+                else continue;
             }
         }
         return "";
